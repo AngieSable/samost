2429fe754dad101d1901ca37cc0aa539456cc556
AngieSable <sobolev.angel@gmail.com>
2025-04-25 13:09:58 +0300

    myproject

---
+++ sr250425/sr250425/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace sr250425
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
